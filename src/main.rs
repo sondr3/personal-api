c548c2c21f51e6d10939f6ab467e9240eb2e73d0
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-30 22:26:43 +0200

    Hello dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use pretty_env_logger;
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    pretty_env_logger::init();
+
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello).run(([0, 0, 0, 0], 8080)).await;
 }
