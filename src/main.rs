8403059a310c3a417c0e904de78f641ed4b19c44
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-10 15:42:44 +0200

    Move to Rocket v0.5 over Warp :D

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+#[macro_use]
+extern crate rocket;
+
 pub mod github;
 
 use anyhow::Result;
 use dotenv::dotenv;
-use log::info;
+use rocket::{Build, Rocket};
 use serde::Deserialize;
-use sqlx::sqlite::SqlitePoolOptions;
-use warp::Filter;
+use sqlx::{sqlite::SqlitePoolOptions, Pool, Sqlite};
 
 use crate::github::GitHub;
 
@@ -16,14 +18,12 @@ struct Env {
     database_url: String,
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    dotenv().ok();
-    pretty_env_logger::init();
+#[get("/hello/<name>")]
+fn hello(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
 
-    let env = envy::from_env::<Env>()?;
-    info!("{:?}", env);
-
+async fn initialize_db(env: &Env) -> Result<Pool<Sqlite>> {
     let pool = SqlitePoolOptions::new()
         .max_connections(5)
         .connect(&env.database_url)
@@ -31,12 +31,32 @@ async fn main() -> Result<()> {
 
     sqlx::migrate!("./migrations").run(&pool).await?;
 
+    Ok(pool)
+}
+
+fn rocket() -> Rocket<Build> {
+    rocket::build().mount("/", routes![hello])
+}
+
+#[rocket::main]
+async fn main() {
+    dotenv().ok();
+
+    let env = match envy::from_env::<Env>() {
+        Ok(env) => env,
+        Err(e) => panic!("{}", e),
+    };
+
+    let _pool = match initialize_db(&env).await {
+        Ok(pool) => pool,
+        Err(e) => panic!("{}", e),
+    };
+
     let mut gh = GitHub::new();
-    gh.update(&env.login, &env.token).await?;
+    gh.update(&env.login, &env.token).await.unwrap();
 
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
-
-    warp::serve(hello).run(([0, 0, 0, 0], 8080)).await;
-
-    Ok(())
+    if let Err(e) = rocket().launch().await {
+        eprintln!("Rocket could not launch: {}", e);
+        drop(e);
+    }
 }
