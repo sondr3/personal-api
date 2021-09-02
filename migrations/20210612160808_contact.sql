2a128b0dc0a5b2348b44d491b793c1861ff6e55f
Sondre Nilsen <sondre@eons.io>
2021-09-02 18:20:17 +0200

    Reformat migration file

--- migrations/20210612160808_contact.sql
+++ migrations/20210612160808_contact.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS contact
+create table if not exists contact
 (
-    id           uuid        NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-    name       text        not null,
+    id           uuid        not null primary key default gen_random_uuid(),
+    name         text        not null,
     sender       text        not null,
     message      text        not null,
-    contacted_at timestamptz NOT NULL             DEFAULT now()
+    contacted_at timestamptz not null             default now()
 );
